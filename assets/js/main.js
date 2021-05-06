d99be839463dfc8d32c95db522fd3ff83e64586c
Stefan Kremenovic <35824199+kremenovic@users.noreply.github.com>
2021-05-06 16:39:27 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 const questions = document.querySelectorAll(".question");
-
+const getInput = document.querySelectorAll(".animated_input input");
+const getLabel = document.querySelectorAll(".animated_input label");
+// FAQ toggle
 questions.forEach((question) => {
   question.addEventListener("click", (e) => {
     const currentQuestion = e.currentTarget;
@@ -10,6 +12,22 @@ questions.forEach((question) => {
         question.classList.remove("active");
       })
       currentQuestion.classList.add("active");
+    }
+  })
+})
+
+// Animation for get started input
+
+getInput.forEach(inputField => {
+  inputField.addEventListener("change", () => {
+    if(inputField.value.length === 0) {
+      getLabel.forEach(label => {
+        label.classList.remove("active_label");
+      })
+    } else {
+      getLabel.forEach(label => {
+        label.classList.add("active_label");
+      })
     }
   })
 })
